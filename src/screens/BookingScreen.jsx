d4db980098d812056c4e7bb3024c296ea67a0bf7
JiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-08-12 00:45:29 +0545

    booking screen

--- src/screens/BookingScreen.jsx
+++ src/screens/BookingScreen.jsx
@@ -1,10 +1,36 @@
 "use client"
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useParams } from "next/navigation";
 
 const BookingScreen = () => {
   const params = useParams();
   const id = params.id;
+
+  const [room, setRoom] = useState([]);
+  const [loading, setLoading] = useState();
+  const [error, setError] = useState();
+
+  const BookRoom = async () => {
+    try {
+        setLoading(true);
+        const response = await axios.post(`/api/rooms`);
+        const { data } = response;
+        setRoom(data.room);
+        setLoading(false);
+        // console.log(data);
+        // console.log(response.data.room);
+      } catch (error) {
+        setError(true);
+        console.log(error);
+        setLoading(false);
+      }
+  }
+
+  useEffect(() => {
+    BookRoom();
+  },[]);
+
+
   return (
     <>
       <div>BookingScreen</div>
